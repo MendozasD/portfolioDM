7dc0bf66840ec5c3c4d5c9567dc9c23b399dc31b
David Mendoza <dmendozasommer@gmail.com>
2022-11-18 16:37:27 +0100

    SVG Files properly created. SVG background setup. Responsiveness ok

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  # This super helper allows me to use External Linked SVG Files :)
+  # <%= embedded_svg('frame-mesh') %> This lins is use to render in HTML
+  def embedded_svg(filename, options = {})
+    assets = Rails.application.assets
+    asset = assets.find_asset(filename)
+
+    if asset
+      file = asset.source.force_encoding("UTF-8")
+      doc = Nokogiri::HTML::DocumentFragment.parse file
+      svg = doc.at_css "svg"
+      svg["class"] = options[:class] if options[:class].present?
+    else
+      doc = "<!-- SVG #{filename} not found -->"
+    end
+
+    raw doc
+  end
 end
